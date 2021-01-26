bc19f60e0387e7510966108059d1e037b2265064
zkobrinsky <zachkobrinsky@gmail.com>
2021-01-26 12:59:22 -0500

    config routes for all paths to serve react, made p5 basic sketch

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
+import Sketch from "../components/Sketch";
 
 export default (
   <Router>
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/draw" component={Sketch} />
+      <Route path="/" component={Home} />
     </Switch>
   </Router>
 );
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static#index'
+  get '/*path' => 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
